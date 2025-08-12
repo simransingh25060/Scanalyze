6d40cd3328d964825bd52fea597c9cca4970ab32
Simran Singh <simransingh25060@gmail.com>
2025-08-12 22:13:54 +0530

    ADD: Navbar and homepage

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router"
+
+const Navbar = () => {
+    return(
+        <nav className="navbar">
+            <Link to="/">
+            <p className="text-2xl font-bold text-gradient">Scanalyze</p>
+            </Link>
+            <Link to="/upoad" className="primary-button w-fit">
+            Upload Resume
+            </Link>
+        </nav>
+    )
+}
+
+export default Navbar
