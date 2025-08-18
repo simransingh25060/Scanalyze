47517ad9f05a3826e6c4858efe4de1980517ce2c
Simran Singh <simransingh25060@gmail.com>
2025-08-19 01:16:47 +0530

    ADD: Resume feedback components

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router";
-import Summary from "~/components/ATS";
-import Details from "~/components/Details"
-import ATS from "~/components/ATS";;
-import { usePuterStore } from "~/lib/puter";
+import {Link, useNavigate, useParams} from "react-router";
+import {useEffect, useState} from "react";
+import {usePuterStore} from "~/lib/puter";
+import Summary from "~/components/Summary";
+import ATS from "~/components/ATS";
+import Details from "~/components/Details";
 
 export const meta = () => ([
-    {title: 'Scanalyze | Review'},
-    {name: 'description', content: 'Detailed overview of your resume.'},
+    { title: 'Scanalyze | Review ' },
+    { name: 'description', content: 'Detailed overview of your resume' },
 ])
 
 const Resume = () => {
@@ -17,7 +17,6 @@ const Resume = () => {
     const [resumeUrl, setResumeUrl] = useState('');
     const [feedback, setFeedback] = useState<Feedback | null>(null);
     const navigate = useNavigate();
-
 
     useEffect(() => {
         if(!isLoading && !auth.isAuthenticated) navigate(`/auth?next=/resume/${id}`);
@@ -50,50 +49,42 @@ const Resume = () => {
         loadResume();
     }, [id]);
 
-
     return (
-       <main className="!pt-0">
-        <nav className="resume-nav">
-           <Link to="/" className="back-button">
-           <img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5"/>
-           <span className="text-gray-800 text-sm font-semibold">Back to Homepage</span>
-           </Link>
-        </nav>
-
-        <div className="flex flex-col w-full max-lg:flex-col-reverse">
-              <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-center">
-
-                {imageUrl && resumeUrl && (
-                    <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
-                         <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
+        <main className="!pt-0">
+            <nav className="resume-nav">
+                <Link to="/" className="back-button">
+                    <img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5" />
+                    <span className="text-gray-800 text-sm font-semibold">Back to Homepage</span>
+                </Link>
+            </nav>
+            <div className="flex flex-row w-full max-lg:flex-col-reverse">
+                <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-center">
+                    {imageUrl && resumeUrl && (
+                        <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
+                            <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
                                 <img
                                     src={imageUrl}
                                     className="w-full h-full object-contain rounded-2xl"
                                     title="resume"
                                 />
                             </a>
-                    </div>
-                )}
-                   
-             </section>
-
-              <section className="feedback-section">
+                        </div>
+                    )}
+                </section>
+                <section className="feedback-section">
                     <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
                     {feedback ? (
                         <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
                             <Summary feedback={feedback} />
                             <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
                             <Details feedback={feedback} />
-                           
                         </div>
                     ) : (
                         <img src="/images/resume-scan-2.gif" className="w-full" />
                     )}
                 </section>
-        </div>
-
-       </main>
+            </div>
+        </main>
     )
 }
-
 export default Resume
